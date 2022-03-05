b305522d8181323e88d043de245ea23f92d96c8b
activedigital <a>
2022-03-05 14:03:21 +0100

    init

---
+++ src/dashboard/StartProgram.java
@@ -0,0 +1,14 @@
+package dashboard;
+
+import utils.Input;
+
+public class StartProgram {
+
+    public static void main(String[] args) {
+
+
+        //checker inputa
+        String input = Input.getInputFromTerminal();
+        System.out.println(input);
+    }
+}
---
+++ src/utils/Input.java
@@ -0,0 +1,14 @@
+package utils;
+
+import java.util.Scanner;
+
+public class Input {
+
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public static String getInputFromTerminal(){
+
+        return scanner.nextLine();
+    }
+}
